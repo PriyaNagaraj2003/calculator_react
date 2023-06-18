8dc9b2679239ba1e13b7916c0ff2aaae03000c9d
mugunth491 <mugunthsumathi123@gmail.com>
2023-06-18 14:19:52 +0530

    all files added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import CalculatorMain from './CalculatorMain'
+import React from "react";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div>
+      <CalculatorMain/>
+    </div>
+  )
+}
+
+export default App
